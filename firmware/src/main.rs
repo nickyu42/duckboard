0a412a215721c671256bd64a0a57ca613bcc0264
Nick Yu <nickyu42@gmail.com>
2023-03-01 22:05:38 +0100

    Refactor

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::{prelude::*};
 // Entry point
 use cortex_m_rt::entry;
 
@@ -106,7 +107,6 @@ fn main() -> ! {
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, vid_pid)
         .manufacturer("Compubotics")
         .product("Duckboard")
-        .serial_number("4242")
         .max_packet_size_0(64)
         .device_class(2)
         .build();
@@ -119,59 +119,7 @@ fn main() -> ! {
         if let Some(r) = encoder.read() {
             debug!("{:?}", r);
         }
-        // debug!("{:?} {:?}", matrix, encoder.read());
-        delay.delay_us(1);
 
-        // hid.pull_raw_output(&mut [0; 64]).ok();
+        hid.pull_raw_output(&mut [0; 64]).ok();
     }
-
-    // let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
-    // let usb_bus = UsbBusAllocator::new(UsbBus::new(
-    //     pac.USBCTRL_REGS,
-    //     pac.USBCTRL_DPRAM,
-    //     clocks.usb_clock,
-    //     true,
-    //     &mut pac.RESETS,
-    // ));
-    // let mut serial = SerialPort::new(&usb_bus);
-    // let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-    //     .manufacturer("Fake company")
-    //     .product("Serial port")
-    //     .serial_number("TEST")
-    //     .max_packet_size_0(64)
-    //     .device_class(2) // from: https://www.usb.org/defined-class-codes
-    //     .build();
-
-    // let mut said_hello = false;
-    // loop {
-    //     if !said_hello && timer.get_counter() >= 2_000_000 {
-    //         said_hello = true;
-    //         let _ = serial.write(b"HelloWorld!\r\n");
-    //     }
-
-    //     if !usb_dev.poll(&mut [&mut serial]) {
-    //         continue;
-    //     }
-
-    //     let mut buf = [0u8; 64];
-    //     let _ = serial.read(&mut buf).map(|count| {
-    //         if count == 0 {
-    //             return;
-    //         }
-
-    //         // Echo back in upper case
-    //         buf.iter_mut().take(count).for_each(|c| {
-    //             if let 0x61..=0x7a = *c {
-    //                 *c &= !0x20;
-    //             }
-    //         });
-
-    //         let mut wr_ptr = &buf[..count];
-    //         while !wr_ptr.is_empty() {
-    //             let _ = serial.write(wr_ptr).map(|len| {
-    //                 wr_ptr = &wr_ptr[len..];
-    //             });
-    //         }
-    //     });
-    // }
 }
